373315addaf33c52a273cbbb828e62991bd379d8
Xhunik Miguel <xhuniktzi@gmail.com>
2021-05-04 00:19:03 -0600

    backend 5.0: django connect

--- frontend/frontend/urls.py
+++ frontend/frontend/urls.py
@@ -19,6 +19,6 @@ from web import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index),
-    path('reports/', views.reports),
+    path('', views.index, name='index'),
+    path('reports/', views.reports, name='reports'),
 ]
--- frontend/web/views.py
+++ frontend/web/views.py
@@ -1,10 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+import requests
 
 # Create your views here.
 
+endpoint = 'http://localhost:5000'
+
 
 def index(request):
-    return render(request, 'index.html')
+    if request.method == 'GET':
+        req_events = requests.get('http://localhost:5000/events')
+        req_stats = requests.get('http://localhost:5000/stats')
+        context = {
+            'input': req_events.text,
+            'output': req_stats.text,
+        }
+        return render(request, 'index.html', context)
+    elif request.method == 'POST':
+        document = request.FILES['document']
+        data = document.read()
+        requests.post('http://localhost:5000/events', data)
+        return redirect('index')
 
 
 def reports(request):
