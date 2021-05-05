8b14ffc9eed9aa96b12f5f5e4906875ddd136cc5
Xhunik Miguel <xhuniktzi@gmail.com>
2021-05-05 15:40:38 -0600

    frontend 4.0

--- frontend/web/views.py
+++ frontend/web/views.py
@@ -23,4 +23,44 @@ def index(request):
 
 
 def reports(request):
-    return render(request, 'reports.html')
+    if request.method == 'GET':
+        date = request.GET.get('date', None)
+        error = request.GET.get('error', None)
+
+        context: dict = {}
+        if date != None:
+            users_by_date = requests.get('http://localhost:5000/stats/by_date',
+                                         {
+                                             'date': date
+                                         }).json()
+
+            context['date_results']: list = []
+            for user in users_by_date['data']:
+                percentage = int((user['cant'] / users_by_date['total']) * 100)
+                context['date_results'].append({
+                    'percentage': percentage,
+                    'user': user['user'],
+                    'cant': user['cant'],
+                    'total': users_by_date['total']
+                })
+        if error != None:
+            errors_by_date = requests.get(
+                'http://localhost:5000/stats/by_error', {
+                    'error': error
+                }).json()
+
+            context['error_results']: list = []
+            for date in errors_by_date['data']:
+                percentage = int(
+                    (date['cant'] / errors_by_date['total']) * 100)
+                context['error_results'].append({
+                    'percentage':
+                    percentage,
+                    'date':
+                    date['date'],
+                    'cant':
+                    date['cant'],
+                    'total':
+                    errors_by_date['total']
+                })
+        return render(request, 'reports.html', context)
